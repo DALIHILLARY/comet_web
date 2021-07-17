dec8e7c2623b5548db539cee23251b30c64531ae
DALI HILLARY <dalirichardh@gmail.com>
2021-07-17 18:38:12 +0300

    disclaimer and login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,14 +31,18 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
+              
 
             <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 flex-left" href="{{ route('register') }}">
+                    Create Account
+                </a> &nbsp; &nbsp; &nbsp; &nbsp;
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
+    
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
--- resources/views/livewire/client/dashboard.blade.php
+++ resources/views/livewire/client/dashboard.blade.php
@@ -1,6 +1,7 @@
-<div>
-    <h3> DASHBOARD </h3>
-    @push('css')
-        <title>DASHBOARD</title>
-    @endpush
+<div style="width: 100%; text-align: center;">
+    <h3> <b>WELCOME TO RATCOMET </b></h3><br>
+    <h5>THIS TOOL is <em><b>NOT</b></em> to be used for any illegal purposes</h5>
+    <p>Ensure that you have rights to the devices on which it will be used </p><br><br>
+    <h4><i>WITH GREAT POWER COMES GREAT RESPONSIBILITY<br>BE SMART !!! .... </i></h4>
+    
 </div>
